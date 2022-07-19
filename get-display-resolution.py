c4aa9d40939f35140d996cb735e870475d0fe8d4
cemreis0 <cemmreis@gmail.com>
2022-07-19 11:54:50 +0300

    removed redundant print function

--- get-display-resolution.py
+++ get-display-resolution.py
@@ -2,6 +2,4 @@ import pyautogui
 
 screenWidth, screenHeight = pyautogui.size()
 
-print(screenWidth, screenHeight)
-
 pyautogui.alert(text=f'Screen Width: {screenWidth}, Screen Height: {screenHeight}', title='Display Resolution')
